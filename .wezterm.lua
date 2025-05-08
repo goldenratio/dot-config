bf61c304b18edce49e2b6b66ba2750acf3c58922
goldenratio <karthikeyanvj@gmail.com>
2025-05-08 19:47:17 +0300

    wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,36 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  font_size = 10.0,
+  color_scheme = "Monokai (dark) (terminal.sexy)",
+  window_padding = {
+    left = 4,
+    right = 4,
+    top = 2,
+    bottom = 2,
+  },
+  -- Set the initial window size (columns x rows)
+  initial_cols = 220,
+  initial_rows = 60,
+  enable_tab_bar = true,
+  hide_tab_bar_if_only_one_tab = true,
+  default_cursor_style = "BlinkingBlock",
+  colors = {
+    cursor_bg = "white",
+    cursor_fg = "black",     -- optional: text color under the cursor
+    cursor_border = "white", -- optional: border color of the cursor
+  },
+  keys = {
+    {
+      key = 'D',
+      mods = 'CTRL',
+      action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
+    },
+    {
+      key = 'd',
+      mods = 'CTRL',
+      action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+    },
+  },
+}
