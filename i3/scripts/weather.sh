fd64f194023f1e94b2ddc31429a1bbf2ef5fa0c7
goldenratio <karthikeyanvj@gmail.com>
2025-06-21 12:53:16 +0300

    update i3 configs

--- i3/scripts/weather.sh
+++ i3/scripts/weather.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-curl -s "https://wttr.in/?format=1"
+curl -s "https://wttr.in/?format=1" | sed 's/^[[:space:]]*//; s/[[:space:]]*$//; s/[[:space:]]\{1,\}/ /g' > /tmp/weather.txt
