6d21e82a8dc6feb33815af63df357ba19087d09c
Karthik VJ <karthikeyanvj@gmail.com>
2025-11-14 12:55:09 +0200

    Enhance tab title formatting and update settings

--- .wezterm_mac.lua
+++ .wezterm_mac.lua
@@ -1,17 +1,46 @@
 local wezterm = require 'wezterm'
 
+-- Helper function to get the tab title
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+  if title and #title > 0 then
+    return title
+  end
+  return tab_info.active_pane.title
+end
+
+-- Define tab_bar table and the format_tab_title function
+local tab_bar = {}
+
+function tab_bar.on_format_tab_title(tab, _tabs, _panes, _config, _hover, _max_width)
+  local zoomed = ''
+  local index = tab.tab_index + 1
+  local title = tab_title(tab)
+  if tab.active_pane.is_zoomed then
+    zoomed = '+'
+  end
+  return {
+    { Text = string.format(' %d %s%s ', index, title, zoomed) }
+  }
+end
+
+-- Hook into wezterm event system
+wezterm.on('format-tab-title', tab_bar.on_format_tab_title)
+
 return {
-  font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
+  font = wezterm.font("JetBrainsMono Nerd Font Mono", {
+    weight = "Regular",
+  }),
   font_size = 14.0,
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   color_scheme = "Monokai (dark) (terminal.sexy)",
+  harfbuzz_features = { "calt=0", "liga=0", "clig=0" },
   window_padding = {
     left = 0,
     right = 0,
     top = 0,
     bottom = 0,
   },
-  use_resize_increments = true,
+  -- use_resize_increments = true,
   use_fancy_tab_bar = true,
   -- Set the initial window size (columns x rows) adsds
   initial_cols = 220,
@@ -23,7 +52,7 @@ return {
     cursor_bg = "white",
     cursor_fg = "black",     -- optional: text color under the cursor
     cursor_border = "white", -- optional: border color of the cursor
-    background = "#1E1E1E",
+    background = "#1e1e1e",
   },
   keys = {
     {
@@ -44,6 +73,16 @@ return {
     {
       key = "]",
       mods = "CMD",
+      action = wezterm.action { ActivatePaneDirection = "Right" },
+    },
+    {
+      key = "LeftArrow",
+      mods = "CMD|SHIFT",
+      action = wezterm.action { ActivatePaneDirection = "Left" },
+    },
+    {
+      key = "RightArrow",
+      mods = "CMD|SHIFT",
       action = wezterm.action { ActivatePaneDirection = "Right" },
     },
     {
@@ -112,6 +151,11 @@ return {
       action = wezterm.action.SendKey { key = "x", mods = "CTRL" },
     },
     {
+      key = "c",
+      mods = "CMD",
+      action = wezterm.action.SendKey { key = "c", mods = "CTRL" },
+    },
+    {
       key = "o",
       mods = "CMD",
       action = wezterm.action.SendKey { key = "o", mods = "CTRL" },
@@ -121,5 +165,42 @@ return {
       mods = "CMD",
       action = wezterm.action.SendKey { key = "i", mods = "CTRL" },
     },
+    {
+      key = "v",
+      mods = "CMD",
+      action = wezterm.action.SendKey { key = "v", mods = "CTRL" },
+    },
+    {
+      key = "w",
+      mods = "CMD",
+      action = wezterm.action.SendKey { key = "w", mods = "CTRL" },
+    },
+    {
+      key = "x",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.ActivateCopyMode,
+    },
+    {
+      key = 'Space',
+      mods = 'CMD|SHIFT',
+      action = wezterm.action.QuickSelect,
+    },
+    {
+      key = "P",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.ActivateCommandPalette,
+    },
+    {
+      key = "v",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.PasteFrom("Clipboard"),
+    },
+    {
+      key = "w",
+      mods = "CMD|SHIFT",
+      action = wezterm.action.CloseCurrentTab { confirm = true }
+    },
+    { key = '{', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(-1) },
+    { key = '}', mods = 'SHIFT|ALT', action = wezterm.action.MoveTabRelative(1) }
   },
 }
